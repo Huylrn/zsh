e62c2bf32b500778dd5e5c055a7ce6f0e4237931
HuyNguyen <141661219+Huylrn@users.noreply.github.com>
2023-11-01 14:27:16 +0700

    up

--- function/update.sh
+++ function/update.sh
@@ -1,32 +1,33 @@
 #update MacOS-github
 function update-MacOS-github(){
-    #Vim
     # cd ~/WorkSpace/MacOS/Vim
     # sudo command rm -rf .vscodevimrc .vimrc
-    cd ~
-    cp -R .vscodevimrc .vimrc ~/WorkSpace/MacOS/Vim
-    cp -R .vim ~/WorkSpace/MacOS/Vim~/WorkSpace/MacOS/VsCode
-
     #Vscode
+    cd ~/Library/Application\ Support/Code/User
+    rsync -a --exclude=.git settings.json keybindings.json ~/WorkSpace/MacOS/VsCode
+
+    cd ~
+    #Vim
+    rsync -a .vscodevimrc .vimrc ~/WorkSpace/MacOS/Vim
+    rsync -a --exclude=.git .vim ~/WorkSpace/MacOS/Vim~/WorkSpace/MacOS/VsCode
+
     # cd ~/WorkSpace/MacOS/VsCode
     # sudo command rm -rf settings.json keybindings.json
-    
-    cp ~/Library/Application\ Support/Code/User/settings.json keybindings.json 
 
     #Config
     # cd ~/WorkSpace/MacOS/Config
     # sudo command rm -rf .config .zshrc
     
-    cp -R .config .zshrc ~/WorkSpace/MacOS/Config
+    rsync -a --exclude=.git .config .zshrc ~/WorkSpace/MacOS/Config
 
     # sudo command rm -rf .oh-my-zsh/custom
     
-    cp -R .oh-my-zsh/custom ~/WorkSpace/MacOS/Config/.oh-my-zsh
+    rsync -a --exclude=.git .oh-my-zsh/custom ~/WorkSpace/MacOS/Config/.oh-my-zsh
 
     #git-cmd
     cd ~/WorkSpace/MacOS
     git diff
     git add .
-    git commit -am "$(date)"
+    git commit -m "$(date)"
     git push
 }
