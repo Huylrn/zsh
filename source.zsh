0bf71c040d913e275aef71ce57a2c16c9485d84b
luna <luna@MacBook-Air-cua-luna.local>
2025-02-09 18:25:57 +0700

    fix rm tool

--- source.zsh
+++ source.zsh
@@ -1,16 +1,20 @@
 # function
-source ~/.config/zsh/function/run.sh                        # run
-source ~/.config/zsh/function/git.sh                        # git
-source ~/.config/zsh/function/search.sh                     # search
-source ~/.config/zsh/function/web-search.sh                 # web-search
-source ~/.config/zsh/function/update.sh                     # update
-source ~/.config/zsh/function/RM/base.rm-Advance.sh         # RM       
-source ~/.config/zsh/function/bat.sh                        # bat
-source ~/.config/zsh/function/clean_library.sh              # clean_library
-source ~/.config/zsh/function/nvim.sh                       # nvim
-source ~/.config/zsh/function/AI.sh                         # completion _ai
-source ~/.config/zsh/function/Completion/_search            # completion _search
-source ~/.config/zsh/function/Completion/_web-search        # completion _web-search
+source ~/.config/zsh/function/run.sh                 # run
+source ~/.config/zsh/function/git.sh                 # git
+source ~/.config/zsh/function/search.sh              # search
+source ~/.config/zsh/function/web-search.sh          # web-search
+source ~/.config/zsh/function/update.sh              # update
+source ~/.config/zsh/function/RM/base.rm-Advance.sh  # RM
+source ~/.config/zsh/function/bat.sh                 # bat
+source ~/.config/zsh/function/clean_library.sh       # clean_library
+source ~/.config/zsh/function/nvim.sh                # nvim
+source ~/.config/zsh/function/AI.sh                  # completion _ai
+source ~/.config/zsh/function/Completion/_search     # completion _search
+source ~/.config/zsh/function/Completion/_web-search # completion _web-search
 
-source ~/.config/zsh/alias.sh                               # alias
-source ~/.config/zsh/config_auto-complete                   # alias
+source ~/.config/zsh/alias.sh             # alias
+source ~/.config/zsh/config_auto-complete # alias
+
+_auto_add_option_rm_Advance=TRUE
+_message_confirm_add_option_rm_Advance=TRUE
+_message_confirm_add_option_rm_Advance=TRUE
